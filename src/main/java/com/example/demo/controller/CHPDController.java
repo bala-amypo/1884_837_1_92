db4157894b2ab0de159c6d64944c83583954835b
bala-amypo <bala@amypo.in>
2025-12-18 06:07:06 +0000

    User commit

--- src/main/java/com/example/demo/controller/CHPDController.java
+++ src/main/java/com/example/demo/controller/CHPDController.java
@@ -1,5 +1,23 @@
-package com.example.demo.controller
+package com.example.demo.controller;
 
-public class CHPDController{
-    public:
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.CHPD;
+import com.example.demo.service.CHPDService;
+
+@RestController
+public class StudentController {
+    @Autowired
+    StudentService studentService;
+
+    @PostMapping("/postdata")
+    public Student postdata(@RequestBody Student student){
+        return studentService.saveStudent(student);
+    }
+    
+    
+
 }
