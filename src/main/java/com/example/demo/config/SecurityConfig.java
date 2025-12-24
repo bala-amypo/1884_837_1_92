1f24c6ddc7bf583ae92585778c730c1aa70c0209
bala-amypo <bala@amypo.in>
2025-12-24 05:17:26 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -15,11 +15,11 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
-    "/swagger-ui/**",
-    "/swagger-ui.html",
-    "/v3/api-docs/**",
-    "/**"
-).permitAll()
+                    "/swagger-ui/**",
+                    "/swagger-ui.html",
+                    "/v3/api-docs/**",
+                    "/**"
+                ).permitAll()
             );
 
         return http.build();
